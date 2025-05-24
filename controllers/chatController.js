14e134feb25ff821175df577ad2d067d8b165fd6
phamhuuloc219 <tranthithanhtien212@gmail.com>
2025-05-24 15:31:48 +0700

    update code

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -3,51 +3,66 @@ const User = require('../models/User');
 
 module.exports = {
     accessChat: async (req, res) => {
-        const {userId} = req.body;
-        
-        if(!userId){
-            return res.status(400).json({message: 'User ID is required'});
+        const { userId } = req.body;
+
+        if (!userId) {
+            return res.status(400).json({ message: 'User ID is required' });
         }
 
-        var isChat = await Chat.find({
-            isGroupChat: false,
-            $and: [
-                {users: {$elemMatch: { $eq: req.user.id}}},
-                {users: {$elemMatch: { $eq: userId}}}
-            ]
-        }).populate("users", "-password").populate("latestMessage");
-
-        isChat = await User.populate(isChat, {
-            path: "latestMessage.sender",
-            select: "username profile email"
-        });
-
-        if(isChat.length > 0 ){
-            res.send(isChat[0]);
-        } else{
-            var ChatData = {
-                chatName: req.user.id,
+        try {
+            let chat = await Chat.find({
                 isGroupChat: false,
-                users: [req.user.id, userId]
-            };
+                $and: [
+                    { users: { $elemMatch: { $eq: req.user.id } } },
+                    { users: { $elemMatch: { $eq: userId } } }
+                ]
+            })
+                .populate("users", "-password")
+                .populate("latestMessage");
 
-            try {
+            chat = await User.populate(chat, {
+                path: "latestMessage.sender",
+                select: "username profile email"
+            });
+
+            if (chat.length > 0) {
+                let fullChat = chat[0].toObject();
+                if (!fullChat.hasOwnProperty('latestMessage')) {
+                    fullChat.latestMessage = null;
+                }
+                return res.status(200).json(fullChat);
+            } else {
+                const ChatData = {
+                    chatName: req.user.id,
+                    isGroupChat: false,
+                    users: [req.user.id, userId]
+                };
+
                 const createdChat = await Chat.create(ChatData);
-                const FullChat = await Chat.findOne({ _id: createdChat._id }).populate("users", "-password");
-                res.status(200).json(FullChat);
-            } catch (error) {
-                res.status(400).json({ message: error.message });
+                let fullChat = await Chat.findOne({ _id: createdChat._id })
+                    .populate("users", "-password")
+                    .populate("latestMessage");
+
+                fullChat = fullChat.toObject();
+                if (!fullChat.latestMessage) {
+                    fullChat.latestMessage = null;
+                }
+
+                return res.status(200).json(fullChat);
             }
+        } catch (error) {
+            return res.status(400).json({ message: error.message });
         }
     },
 
+
     getChat: async (req, res) => {
         try {
-            Chat.find({users: {$elemMatch: { $eq: id}}})
+            Chat.find({ users: { $elemMatch: { $eq: id } } })
                 .populate("users", "-password")
                 .populate("groupAdmin", "-password")
                 .populate("latestMessage")
-                .sort({updatedAt: -1})
+                .sort({ updatedAt: -1 })
                 .then(async (results) => {
                     results = await User.populate(results, {
                         path: "lastMessage.sender",
